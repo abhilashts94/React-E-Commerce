5da07227de63fc3f59f3a5174939bb379dd86f3f
abhilash <abhilashts94@gmail.com>
2021-10-20 20:37:13 +0530

    updated on oct 20 evng

--- src/index.js
+++ src/index.js
@@ -8,13 +8,25 @@ import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
 
+// dev-cq8lto8n.us.auth0.com
+// v0lasJl2xIXIPSzU93MQpj7gkgE81oGt
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain='dev-cq8lto8n.us.auth0.com'
+    clientId='v0lasJl2xIXIPSzU93MQpj7gkgE81oGt'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
   document.getElementById('root')
 )
